bdf7723220655304d7cc50ea50574679e2a4ca7c
leonel47 <waboleonel@gmail.com>
2023-03-20 10:59:58 +0100

    Registering new employee

--- src/main/java/com/wbt/sp3reactive/controller/HomeController.java
+++ src/main/java/com/wbt/sp3reactive/controller/HomeController.java
@@ -1,21 +1,29 @@
 package com.wbt.sp3reactive.controller;
 
 import com.wbt.sp3reactive.model.Employee;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class HomeController {
 
-    private final Map<String, Employee> DATABASE = Map.of(
-            "nero", new Employee("nero", "ka"),
-            "anna", new Employee("anna", "sm")
-    );
+    private final Map<String, Employee> DATABASE = new HashMap<>();
+
+
+    @PostConstruct
+    private void initialize() {
+        DATABASE.put("nero", new Employee("nero", "ka"));
+        DATABASE.put("anna", new Employee("anna", "sm"));
+    }
 
     @GetMapping(path = {"/"})
     public Mono<Rendering> index() {
@@ -24,8 +32,17 @@ public class HomeController {
                 .map(employees -> Rendering
                         .view("index")
                         .modelAttribute("employees", employees)
+                        .modelAttribute("newEmployee", new Employee("", ""))
                         .build()
                 );
+    }
+
+    @PostMapping(path = {"/new-employee"})
+    public Mono<String> newEmployee(final @ModelAttribute Mono<Employee> newEmployee) {
+        return newEmployee.map(employee -> {
+            DATABASE.put(employee.firstName(), employee);
+            return "redirect:/";
+        });
     }
 
 }
